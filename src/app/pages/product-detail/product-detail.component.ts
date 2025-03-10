38326dead0c1d1d950b9e938373cf00175181801
Florent Garcia <florent.garcia@oclock.school>
2025-03-10 19:38:23 +0100

    add a service to stock cart in local storage and make it functional

--- src/app/pages/product-detail/product-detail.component.ts
+++ src/app/pages/product-detail/product-detail.component.ts
@@ -1,9 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  afterNextRender,
+  Component,
+  Inject,
+  OnInit,
+  PLATFORM_ID,
+  signal,
+} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { log } from 'console';
 import { ProductService } from '../../services/product.service';
 import { ProductCard } from '../../models/product-card';
-import { CommonModule } from '@angular/common';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-product-detail',
@@ -12,14 +20,32 @@ import { CommonModule } from '@angular/common';
   styleUrl: './product-detail.component.css',
 })
 export class ProductDetailComponent implements OnInit {
+  private isBrowser!: boolean;
   constructor(
     private route: ActivatedRoute,
-    private productService: ProductService
-  ) {}
+    private productService: ProductService,
+    private cartService: CartService,
+    @Inject(PLATFORM_ID) private platformId: object
+  ) {
+    this.isBrowser = isPlatformBrowser(platformId);
+  }
   productSlug: string | null = null;
-  product: ProductCard | undefined = undefined;
+  initialProduct = {
+    id: 0,
+    title: 'string',
+    image: 'string',
+    slug: 'string',
+    latinName: 'string',
+    style: 'string',
+    price: 0,
+    age: 0,
+    description: 'string',
+  };
+  product = signal<ProductCard>(this.initialProduct);
+  isInCart = signal<boolean>(false);
 
   ngOnInit(): void {
+    //Get slug from URL and load the product data
     this.route.paramMap.subscribe((params) => {
       this.productSlug = params.get('slug');
       if (this.productSlug) {
@@ -29,17 +55,42 @@ export class ProductDetailComponent implements OnInit {
   }
 
   loadProduct(slug: string): void {
-    console.log(this.productSlug);
+    // Call the method in cartService to fetch data
     this.productService.getProductBySlug(slug).subscribe({
       next: (product: ProductCard | undefined) => {
-        this.product = product;
+        if (product) {
+          // Stock data in a signal
+          this.product.set(product);
+          // Load the cart after arriving in the browser
+          if (this.isBrowser && this.product()) {
+            this.cartService.loadCart();
+            // Verify if the actual product is already in the cart and stock the answer in a signal
+            this.isInCart.set(
+              this.cartService
+                .cart()
+                .some((item) => item.id === this.product().id)
+            );
+          }
+        }
       },
       error: (error) => {
         console.error('Error loading product:', error);
       },
       complete: () => {
-        console.log('Chargement réussi', this.product);
+        console.log('Chargement réussi', this.product());
       },
     });
+  }
+  addToCart(product: ProductCard) {
+    // Add the actual product in the cart and stock the information in the signal
+    this.cartService.addToCart(product);
+    this.isInCart.set(true);
+  }
+
+  removeFromCart(product: ProductCard) {
+    // Remove the actual product in the cart and stock the information in the signal
+
+    this.cartService.removeFromCart(product);
+    this.isInCart.set(false);
   }
 }
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,9 +1,36 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
+import { ProductCard } from '../models/product-card';
+import { error } from 'node:console';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartService {
+  constructor() {}
+  cart = signal<ProductCard[]>([]);
 
-  constructor() { }
+  loadCart() {
+    // Cart saved in localstorage in stocked in the signal "cart"
+    const storedCart = localStorage.getItem('cart');
+    storedCart ? this.cart.set(JSON.parse(storedCart)) : [];
+  }
+
+  addToCart(product: ProductCard): void {
+    // As product is unique, user can put it just once in the cart
+    if (this.cart().some((item) => item === product)) {
+      return;
+    }
+    // Store in the cart signal and in the local storage
+    this.cart.set([...this.cart(), product]);
+    localStorage.setItem('cart', JSON.stringify(this.cart()));
+  }
+
+  removeFromCart(product: ProductCard): void {
+    this.cart.set(this.cart().filter((item) => item.id !== product.id));
+    localStorage.setItem('cart', JSON.stringify(this.cart()));
+  }
+
+  clearCart(): void {
+    localStorage.removeItem('cart');
+  }
 }
